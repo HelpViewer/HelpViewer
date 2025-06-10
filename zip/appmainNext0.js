9f93a975be3872816c4c60763248b30aa0e37701
VSimon <vita.simon@centrum.cz>
2025-06-10 12:30:50 +0200

    Loading logic updated : language marker symbol in path is __ instead of |

--- zip/appmainNext0.js
+++ zip/appmainNext0.js
@@ -1,7 +1,7 @@
 const PAR_NAME_DOC = 'd'; // Help file path
 const urlParams = new URLSearchParams(window.location.search);
 
-dataPath = urlParams.get(PAR_NAME_DOC)?.replace('|', '');
+dataPath = urlParams.get(PAR_NAME_DOC)?.replace('__', '');
 
 if (typeof dataPath !== 'string' || dataPath.trim() === '') {
   dataPath = FILENAME_DEFAULT_HELPFILE;
