29a8dec2a8edde4e8b6a255e4651489ce3af6c19
VSimon <vita.simon@centrum.cz>
2025-06-15 12:22:00 +0200

    Data sources refactor - ability to read ZIP and disk folders since now

--- hvdata/appmain.js
+++ hvdata/appmain.js
@@ -3,14 +3,21 @@ const STO_HELP = 'STO_HELP';
 const STOF_TEXT = 'text';
 const STOF_B64 = 'base64';
 
+const STORAGE_ENGINES = {
+  '.zip': async (path) => newStorageZip(path),
+  '/': async (path) => newStorageDir(path),
+};
+
 var _Storage = (() => {
   var storagesC = new Map();
 
   async function add(key, path) {
-    const zip = await ZIPHelpers.loadZipFromUrl(path);
-    if (zip) {
-      storagesC.set(key, zip);
-      return true;
+    for (const keyC in STORAGE_ENGINES) {
+      if (path.endsWith(keyC)) {
+        const eng = await STORAGE_ENGINES[keyC](path);
+        storagesC.set(key, eng);
+        return true;
+      }
     }
     return null;
   }
@@ -19,16 +26,38 @@ var _Storage = (() => {
     if (!storagesC.has(key))
       return null;
 
-    return await ZIPHelpers.searchArchiveForFile(filePath, storagesC.get(key), format);
+    return await storagesC.get(key).search(filePath, format);
   }
 
   async function getSubdirs(key, parentPath) {
     if (!storagesC.has(key))
       return [];
 
+    return await storagesC.get(key).getSubdirs(parentPath);
+  }
+
+  return {
+    add,
+    search,
+    getSubdirs
+  };
+})();
+
+async function newStorageZip(path) {
+  var storageO = await init(path);
+
+  async function init(path) {
+    return await ZIPHelpers.loadZipFromUrl(path);
+  }
+
+  async function search(filePath, format = STOF_TEXT) {
+    return await ZIPHelpers.searchArchiveForFile(filePath, storageO, format);
+  }
+
+  async function getSubdirs(parentPath) {
     const subdirs = new Set();
 
-    storagesC.get(key).forEach((relativePath, file) => {
+    storageO?.forEach((relativePath, file) => {
       if (relativePath.startsWith(parentPath) && relativePath !== parentPath) 
       {
         const subPath = relativePath.slice(parentPath.length);
@@ -46,11 +75,70 @@ var _Storage = (() => {
   }
 
   return {
-    add,
     search,
     getSubdirs
   };
-})();
+}
+
+async function newStorageDir(path) {
+  var storageO = await init(path);
+
+  async function init(path) {
+    return path.replace('/', '');
+  }
+  async function search(filePath, format = STOF_TEXT) {
+    const fpath = `${storageO}/${filePath}`;
+    const response = await fetchDataOrEmpty(fpath);
+
+    switch (format) {
+      case STOF_B64:
+        const zip = new JSZip();
+        const fname = "a.txt";
+        zip.file(fname, response, { compression: "STORE" });
+        const base64zip = await zip.generateAsync({ type: STOF_B64 });
+        await zip.loadAsync(base64zip, { base64: true });
+        const file = zip.file(fname);
+        const b64Data = await file.async(STOF_B64);
+        return b64Data;
+      case STOF_TEXT:
+      default:
+        return toText(response);
+    }
+  }
+
+  function toText(ab) {
+    const decoder = new TextDecoder("utf-8");
+    const text = decoder.decode(ab);
+    return text;
+  }
+
+  async function getSubdirs(parentPath) {
+    const list = search(`${storageO}/${parentPath}/__dir.lst`, format = STOF_TEXT);
+    const text = toText(list);
+    text = text.trim().replace(/\r\n/g, "\n").split('\n');
+
+    const subdirs = new Set();
+    text?.forEach((line, index) => {
+      subdirs.add(line);
+    });
+
+    return [...subdirs];
+  }
+
+  async function fetchDataOrEmpty(url) {
+    try {
+      const response = await fetchData(url);
+      return response;
+    } catch (error) {
+      return new ArrayBuffer(0);
+    }
+  }
+
+  return {
+    search,
+    getSubdirs
+  };
+}
 
 async function main() {
   var st = await _Storage.add(STO_DATA, 'hvdata/data.zip');
@@ -59,13 +147,21 @@ async function main() {
   runApp();
 }
 
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    const arrayBuffer = await response.arrayBuffer();
+    return arrayBuffer;
+  } catch (error) {
+    throw error;
+  }
+}
+
 const ZIPHelpers = (() => {
   async function loadZipFromUrl(url) {
     try {
-      const response = await fetch(url);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const arrayBuffer = await response.arrayBuffer();
-  
+      const arrayBuffer = await fetchData(url);
       const arch = await JSZip.loadAsync(arrayBuffer);
       return arch;
     } catch (error) {
