26765112c38068b456ba1f5b221fe4f89e4b8de8
VSimon <vita.simon@centrum.cz>
2025-07-27 19:01:11 +0200

    Plugin : puiNavigation : navigation send event

--- zip/plugins/puiNavigation.js
+++ zip/plugins/puiNavigation.js
@@ -1,3 +1,13 @@
+class NavigationMove extends IEvent {
+  constructor() {
+    super();
+    this.treeId = null;
+    this.previousId = 0;
+    this.newId = 0;
+    this.direction = undefined;
+    this.paramIdName = undefined;
+  }
+}
 class puiNavigation extends IPlugin {
     constructor(aliasName, data) {
       super(aliasName, data);
@@ -12,8 +22,12 @@ class puiNavigation extends IPlugin {
     static buttonLeft;
     static buttonTop;
     static buttonRight;
-  
+
+    static EVT_NAV_MOVE = NavigationMove.name;
+
     init() {
+      puiNavigation.eventDefinitions.push([puiNavigation.EVT_NAV_MOVE, NavigationMove, null]); // outside event handlers
+
       const baseId = this.aliasName;
       const target = this.config[puiNavigation.KEY_CFG_TARGET] || 'header';
       const treeId = this.config[puiNavigation.KEY_CFG_TREE_ID] || 'tree';
@@ -39,15 +53,28 @@ class puiNavigation extends IPlugin {
         toggleVisibility(puiNavigation.buttonTop, !(i <= 1));
       }
 
-      const _buttonAction = (evt, next) => {
+      const _buttonAction = (evt, next, direction) => {
+        const current = getId();
         evt.event.preventDefault();
         setToHrefByValues((d) => d.kvlist.set(parIdName, next));
         updateNavButtons(next);
+        
+        if (next == current)
+          return;
+
+        sendEvent(puiNavigation.EVT_NAV_MOVE, (data) => {
+          data.id = this.aliasName;
+          data.treeId = treeId;
+          data.previousId = current;
+          data.newId = next;
+          data.direction = direction;
+          data.paramIdName = parIdName;
+        });
       }
 
       const _buttonActionLeft = (evt) => {
         const next = getId()-1;
-        _buttonAction(evt, next);
+        _buttonAction(evt, next, -1);
       }
       puiNavigation.buttonLeft = uiAddButton(idLeft, '⬅', target, _buttonActionLeft);
   
@@ -57,13 +84,13 @@ class puiNavigation extends IPlugin {
         var next = parseInt(treeItem.parentElement.parentElement.parentElement.querySelector('summary > a').id.slice(treeId.length + 1));
         if (current == next)
           next = 1;
-        _buttonAction(evt, next);
+        _buttonAction(evt, next, 0);
       }
       puiNavigation.buttonTop = uiAddButton(idTop, '⬆', target, _buttonActionTop);
 
       const _buttonActionRight = (evt) => {
         const next = getId()+1;
-        _buttonAction(evt, next);
+        _buttonAction(evt, next, 1);
       }
       puiNavigation.buttonRight = uiAddButton(idRight, '➡', target, _buttonActionRight);
 
