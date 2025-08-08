b3974a035a95ac3f92a03aae22b32edbbafe8174
VSimon <vita.simon@centrum.cz>
2025-08-08 21:24:24 +0200

    Plugin : pTRTriage : fix of exception for incomplete uri address

--- zip/plugins/pTRTriage.js
+++ zip/plugins/pTRTriage.js
@@ -12,16 +12,39 @@ class pTRTriage extends pTRPhasePlugin {
   }
 
   onETShowChapterResolutions(r) {
+    r.setTitle(r.heading);
+
     if (r.fileMedium === UserDataFileLoadedFileType.NETWORK) {
-      log(`Resolution: ${r.uri} ... is external resource link.`);
+      log(`Resolution: ${r.eventId};${r.uri} ... is external resource link.`);
       return;
     }
 
-    const uriRelative = new URL(r.uri).search;
+    var uriRelative;
+
+    try {
+      uriRelative = new URL(r.uri).search;
+    } catch (error) {
+      uriRelative = r.uri;
+    }
 
     if (uriRelative && uriRelative.startsWith(`?${PAR_NAME_DOC}=`)) {
-      log(`Resolution: ${r.uri} ... is external help file.`);
+      log(`Resolution: ${r.eventId};${r.uri} ... is external help file.`);
       return;
+    }
+
+    if (r.uri?.startsWith(":")) {
+      r.getStorageData = r.getAppData;
+      log(`Resolution: ${r.eventId};${r.uri} ... main storage set from ${STO_HELP} to ${STO_DATA}.`);
+      r.uri = r.uri.substring(1);
+    }
+
+    if (r.uri?.startsWith("~")) {
+      r.getStorageData = pTopicRenderer.STORAGE_NETW;
+      log(`Resolution: ${r.eventId};${r.uri} ... main storage set from ${STO_HELP} to STORAGE_NETWORK.`);
+      r.uri = r.uri.substring(1);
+      r.getStorageData(r.uri).then((x) => {
+        r.containerContent.innerHTML = x;
+      });
     }
 
     r.preventDefault();
