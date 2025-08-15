30fcee7bbc40d36528054075682d9ac5f746cb14
VSimon <vita.simon@centrum.cz>
2025-08-16 01:41:22 +0200

    Plugin : puiButtonAsBook : fix of empty pages on the end

--- zip/plugins/puiButtonAsBook.js
+++ zip/plugins/puiButtonAsBook.js
@@ -42,21 +42,22 @@ class puiButtonAsBook extends puiButton {
 
     files.forEach((x) => {
       prom = prom.then(() => (x.startsWith(':') ? storageSearch(STO_DATA, x.substring(1)) : storageSearch(STO_HELP, x)).then((y) => {
-        if (y.length > 0)
+        if (y.length > 0) {
           textOfFiles += '\n' + y + '\n';
 
-        if (x == homeData) {
-          const metainfo = `\n| ${_T('helpfile')} | ${_T('version')} |\n|---|---|\n| ${configGetValue(CFG_KEY__PRJNAME)} | ${configGetValue(CFG_KEY__VERSION)} |\n| ${configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT)} | ${configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT)} |\n`;
-          textOfFiles += metainfo;
-          textOfFiles += DIRECTIVE_PRINT_PAGEBREAK;
-          textOfFiles += metainfo;
-          //textOfFiles += `\n<b>${_T('helpfile')}: ${configGetValue(CFG_KEY__PRJNAME)} <br>${_T('version')}: ${configGetValue(CFG_KEY__VERSION)}</b>`;
-          const data = $('tree')?.outerHTML;
+          if (x == homeData) {
+            const metainfo = `\n| ${_T('helpfile')} | ${_T('version')} |\n|---|---|\n| ${configGetValue(CFG_KEY__PRJNAME)} | ${configGetValue(CFG_KEY__VERSION)} |\n| ${configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT)} | ${configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT)} |\n`;
+            textOfFiles += metainfo;
+            textOfFiles += DIRECTIVE_PRINT_PAGEBREAK;
+            textOfFiles += metainfo;
+            //textOfFiles += `\n<b>${_T('helpfile')}: ${configGetValue(CFG_KEY__PRJNAME)} <br>${_T('version')}: ${configGetValue(CFG_KEY__VERSION)}</b>`;
+            const data = $('tree')?.outerHTML;
 
-          if (data && data.length > 0)
-            textOfFiles += '\n' + data + '\n' + DIRECTIVE_PRINT_PAGEBREAK + '\n';
-        } else {
-          textOfFiles += DIRECTIVE_PRINT_PAGEBREAK + '\n';
+            if (data && data.length > 0)
+              textOfFiles += '\n' + data + '\n' + DIRECTIVE_PRINT_PAGEBREAK + '\n';
+          } else {
+            textOfFiles += DIRECTIVE_PRINT_PAGEBREAK + '\n';
+          }
         }
 
         return Promise.resolve();  
