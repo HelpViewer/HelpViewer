c197c03fbe0f3aa86fec115758ca459e73477cb7
VSimon <vita.simon@centrum.cz>
2025-09-19 01:41:16 +0200

    puiButtonCustPackage : sizes sum and recompute on checkbox checked changed

--- zip/plugins/puiButtonCustPackage.js
+++ zip/plugins/puiButtonCustPackage.js
@@ -30,36 +30,88 @@ class puiButtonCustPackage extends puiButton {
     const downloadButtonID = 'DWNL-' + TI.aliasName;
     var tree = $(treeId);
     if (!tree) {
+      this.keyAndSizes = new Map();
       const contentPane = $(TI.cfgIdContent);
       const buttonInvert = document.createElement("button");
       buttonInvert.innerHTML = '🔄';
-      buttonInvert.onclick = () => Array.from($A('input', tree)).forEach(x => x.checked = !x.checked);
+      buttonInvert.onclick = () => {
+        Array.from($A('input', tree)).forEach(x => x.checked = !x.checked);
+        this.checkboxChanged();
+      }
       const buttonDownload = document.createElement("button");
       buttonDownload.innerHTML = TI.cfgCaption;
-      buttonDownload.onclick = () => {
-        log('E CHCK', Array.from($A('input', tree)).filter(x => x.checked).map(x => x.id));
-      }
+      buttonDownload.onclick = () => this._preparePackage(Array.from($A('input', tree)).filter(x => x.checked).map(x => x.id));
       buttonDownload.id = downloadButtonID;
-
       setHeader(_T('downP-CustPackage'));
       contentPane.innerHTML = `<ul id='${treeId}' class='tree'>${linesToHtmlTree(TI.partsTree, treeId)}</ul>`;
+      contentPane.append(document.createElement("hr"));
+      const spanSumId = 'spanSum';
+      var spanSum = document.createElement("div");
+      spanSum.id = spanSumId;
+      spanSum.innerText = '0 kB';
+      contentPane.append(spanSum);
+      spanSum = $(spanSumId);
       contentPane.append(buttonInvert);
       contentPane.append(buttonDownload);
       tree = $(treeId);
+      this.tree = tree;
       openSubtree(tree);
       $A('a', tree).forEach(a => {
+        const plugins = TI.config[`P-${a.textContent}`].split(';').map(x => x.split(':')[0]).filter(x => x).map(x => `plugins/${x}.js`).join(';');
+        const files = ([TI.config[`F-${a.textContent}`], plugins]).filter(x => x).join(';');
+
+        this.keyAndSizes.set(a.textContent, {
+          fileList : files,
+          r : new Resource('', undefined, STO_DATA, files),
+        });
+
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.checked = true;
         const label = document.createElement("label");
         checkbox.id = a.textContent;
+        checkbox.addEventListener("change", this.checkboxChanged.bind(this));
         label.appendChild(checkbox);
         label.appendChild(document.createTextNode((TI.config[`I-${a.textContent}`] || '🧩') + ' ' + _T(a.textContent)));
         a.replaceWith(label);
-      });  
+      });
+
+      var sizeSum = 0;
+
+      Array.from($A('input', tree)).forEach(a => {
+        this.waitForValue(this.keyAndSizes.get(a.id).r, '_fileLength').then(re => {
+          const oneSize = valKiBs(re);
+          a.parentNode.childNodes[1].nodeValue += ` (${oneSize} kB)`;
+          sizeSum += oneSize;
+          spanSum.innerText = sizeSum + ' kB';
+        })
+      });
     } else {
       $(downloadButtonID).click();
     }
+  }
+
+  _preparePackage(items) {
+    alert('Ahoj');
+    log('E CHCK', items);
+  }
+
+  checkboxChanged(event) {
+    const selected = Array.from($A('input', this.tree)).filter(x => x.checked).map(x => x.id);
+    const sum = [...this.keyAndSizes.entries()].reduce((acc, [key, value]) => 
+      selected.includes(key) ? acc + value.r._fileLength : acc, 0);
+    $('spanSum').innerText = valKiBs(sum) + ' kB';
+  }
+
+  waitForValue(resource, key) {
+    return new Promise(resolve => {
+      const interval = setInterval(() => {
+        if (resource[key] !== 0) {
+          clearInterval(interval);
+          resolve(resource[key]);
+        }
+      }, 100);
+    });
   }
 }
 
