0c76680d85758d47d7c7b7a35c212b66e2a29e33
VSimon <vita.simon@centrum.cz>
2025-09-20 18:03:01 +0200

    puiPanel : added ability to have position top, configuration of top/bottom position above base element id, names refactor

--- zip/plugins/puiPanel.js
+++ zip/plugins/puiPanel.js
@@ -1,20 +1,43 @@
 class puiPanel extends IPlugin {
-  static addition = '<div class="header" id="%%-bottomPanel"><div id="%%-toolbarBottom" role="navigation"></div></div>';
+  static addition = '<div class="header" id="%%-panel"><div id="%%-toolbar" role="navigation"></div></div>';
+
+  static KEY_CFG_POSITION = 'POSITION';
+  static KEY_CFG_BASEELEMENTID = 'BASEELEMENTID';
 
   init() {
     const T = this.constructor;
     const TI = this;
 
-    const containerMain = $('main');
+    const positions = {
+      t: 't',
+      b: 'b',
+    };
+
+    this.DEFAULT_KEY_CFG_POSITION = positions.b;
+    this.DEFAULT_KEY_CFG_BASEELEMENTID = 'content';
+
+    TI.cfgPosition = TI.config[T.KEY_CFG_POSITION] || TI.DEFAULT_KEY_CFG_POSITION;
+    TI.cfgBaseElementId = TI.config[T.KEY_CFG_BASEELEMENTID] || TI.DEFAULT_KEY_CFG_BASEELEMENTID;
+
+    const containerRelativeTo = $(TI.cfgBaseElementId);
+    const parent = containerRelativeTo.parentElement;
     const tmpDiv = document.createElement('div');
     tmpDiv.innerHTML = T.addition.replace(new RegExp('%%', 'g'), this.aliasName);
     const node = tmpDiv.firstChild;
-    if (containerMain && node)
-      containerMain.append(node);
+    
+    if (containerRelativeTo && node) {
+      if (TI.cfgPosition == positions.t) {
+        parent.insertBefore(node, containerRelativeTo);
+      } else 
+      if (TI.cfgPosition == positions.b) {
+        parent.insertBefore(node, containerRelativeTo.nextSibling);
+      }
+    }
+
     this.panel = node;
     this.panel.classList.add(C_HIDDENC);
 
-    const toolbar = $(this.aliasName + '-toolbarBottom');
+    const toolbar = $(this.aliasName + '-toolbar');
 
     const baseButtonAccept = createButtonAcceptHandler(TI, toolbar);
     this.handlerButtonSend = (x) => {
