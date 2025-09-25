7ff028b10017a5a497ee044ee178190096c05ce5
VSimon <vita.simon@centrum.cz>
2025-09-25 11:37:51 +0200

    pColorTheme : refactor, configuration variables extended

--- zip/plugins/pColorTheme.js
+++ zip/plugins/pColorTheme.js
@@ -20,68 +20,53 @@ class pColorTheme extends IPlugin {
     super(aliasName, data);
 
     this.DEFAULT_KEY_CFG_STOREKEY = 'colorTheme';
+    this.DEFAULT_KEY_CFG_COLORMODES = 'inStandard;inGray;inBlackWhite;inBWDuoColor;inSepia';
+    this.DEFAULT_KEY_CFG_DEFAULT = 'inStandard';
   }
 
   init() {
     const T = this.constructor;
     const TI = this;
+
+    TI.targetElement = document.body;
+    TI.cfgCOLORMODES = TI.cfgCOLORMODES.split(';');
     
     const h_EVT_CT_GET = (data) =>
-      data.result = T.ColorTheme.getCurrentColorMode();
+      data.result = TI.getCurrentColorMode();
     TI.eventDefinitions.push([T.EVT_CT_GET, ColorThemeGet, h_EVT_CT_GET]);
 
     const h_EVT_CT_SET = (data) =>
-      T.ColorTheme.setColorMode(data.name);
+      TI.setColorMode(data.name);
     TI.eventDefinitions.push([T.EVT_CT_SET, ColorThemeSet, h_EVT_CT_SET]);
 
     TI.catalogizeEventCall(h_EVT_CT_GET, EventNames.UserConfigGet);
     TI.catalogizeEventCall(h_EVT_CT_SET, EventNames.UserConfigSet);
 
     super.init();
-    T.ColorTheme.init(this.cfgSTOREKEY);
+    const val = TI.getCurrentColorMode();
+    setColorMode(val);
   }
-  
-  /*S: Feature: Set color theme */
-  static ColorTheme = (() => {
-    var KEY_LS_COLORTHEME = undefined;
-    
-    // reimplement yourself
-    var colorModes = ["inStandard", "inGray", "inBlackWhite", "inBWDuoColor", "inSepia"];
-    var defaultColorTheme = colorModes[0];
-    
-    var targetElement = document.body;
-  
-    function init(key) {
-      KEY_LS_COLORTHEME = key;
-      const val = getCurrentColorMode();
-      setColorMode(val);
+
+  getCurrentColorMode() {
+    const val = getUserConfigValue(this.cfgSTOREKEY) || this.cfgDEFAULT;
+    return val;
+  }
+
+  setColorMode(val) {
+    if (!val) {
+      this._switchColorMode();
+      return;
     }
-  
-    function getCurrentColorMode() {
-      const val = getUserConfigValue(KEY_LS_COLORTHEME) || defaultColorTheme;
-      return val;
-    }
-    
-    function switchColorMode() {
-      const base = targetElement;
-      const idx = (colorModes.indexOf(base.classList[0]) + 1) % colorModes.length;
-      setColorMode(colorModes[idx]);
-    }
-    
-    function setColorMode(val) {
-      if (!val) {
-        switchColorMode();
-        return;
-      }
-  
-      const base = targetElement;
-      base.className = val;
-      setUserConfigValue(KEY_LS_COLORTHEME, val);
-    }
-  
-    return { init, switchColorMode, setColorMode, getCurrentColorMode };
-  })();
-  /*E: Feature: Set color theme */
+
+    this.targetElement.className = val;
+    setUserConfigValue(this.cfgSTOREKEY, val);
+  }
+
+  _switchColorMode() {
+    const colorModes = this.cfgCOLORMODES;
+    const idx = (colorModes.indexOf(this.targetElement.classList[0]) + 1) % colorModes.length;
+    this.setColorMode(colorModes[idx]);
+  }
 }
 
 Plugins.catalogize(pColorTheme);
