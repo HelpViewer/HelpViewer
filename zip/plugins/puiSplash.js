71f981c54bbdc7d985314d2b47340a465ad0c66f
VSimon <vita.simon@centrum.cz>
2025-09-26 12:58:13 +0200

    puiSplash : fix of 2 empty pages in pre print view

--- zip/plugins/puiSplash.js
+++ zip/plugins/puiSplash.js
@@ -5,7 +5,7 @@ class puiSplash extends IPlugin {
     this.DEFAULT_KEY_CFG_RELATIVE = 'container';
     this.DEFAULT_KEY_CFG_FILENAME = '_splash.md';
     this.DEFAULT_KEY_CFG_IDNAME = 'splash';
-    this.DEFAULT_KEY_CFG_CSSCLASS = 'splash';
+    this.DEFAULT_KEY_CFG_CSSCLASS = 'splash;hiddenprint';
     this.eventIdStrict = true;
 
     this.show = (file) => showChapter(undefined, undefined, file, undefined, undefined, undefined, this.splashPanel.id);
@@ -50,7 +50,7 @@ class puiSplash extends IPlugin {
         if (c) {
           const sp = document.createElement('div');
           TI.splashPanel = sp;
-          sp.classList.add(TI.cfgCSSCLASS);
+          sp.classList.add(...TI.cfgCSSCLASS.split(';'));
           sp.id = TI.cfgIDNAME;
           relative.parentElement.insertBefore(sp, relative);
           TI.show(TI.cfgFILENAME);
