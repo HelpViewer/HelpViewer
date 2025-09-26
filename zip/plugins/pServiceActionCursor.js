439374f15f3d9779a5631c46452fb658b36616b0
VSimon <vita.simon@centrum.cz>
2025-09-27 00:53:30 +0200

    pServiceActionCursor : service which will enrich system cursor with icon which follows cursor according to mouse move, mouse click of this plugin will create EVENTBUSEVENT system event

---
+++ zip/plugins/pServiceActionCursor.js
@@ -0,0 +1,92 @@
+class SetActionCursor extends IEvent {
+  constructor() {
+    super();
+    this.cursorAddition = '';
+    this.convertedEventId = '';
+    this.offsetLT = [0, 8];
+  }
+}
+
+class ActionClickedEvent extends ClickedEvent {
+  constructor() {
+    super();
+    this.senderId = '';
+  }
+}
+
+class pServiceActionCursor extends pClickConverter {
+  static EVT_AC_SET = SetActionCursor.name;
+  static EVT_AC_STOP = 'StopActionCursor';
+
+  constructor(aliasName, data) {
+    super(aliasName, data);
+
+    this.DEFAULT_KEY_CFG_SYSOBJECT = 'document';
+    this.DEFAULT_KEY_CFG_EVENTBUSEVENT = 'ActionClickedEvent';
+  }
+
+  init() {
+    const T = this.constructor;
+    const TI = this;
+
+    ActionClickedEvent.register(TI.cfgEVENTBUSEVENT);
+
+    const h_EVT_AC_SET = (data) => {
+      TI.targetSysObject = SystemEventHandler.getTargetFromName(TI.cfgSYSOBJECT);
+      TI.SEVT_MOUSE = new SystemEventHandler('', undefined, TI.targetSysObject, 'mousemove', this._mouseMove.bind(this));
+      TI.SEVT_MOUSE.init();
+
+      TI.cursorAddon = document.createElement('span');
+      TI.cursorAddon.id = 'span-' + newUID();
+      TI.cursorAddon.classList.add('cursorAddon');
+      TI.cursorAddon.innerHTML = data.cursorAddition;
+      TI.activeEvent = data;
+      TI.config['EVENTBUSEVENTID'] = data.convertedEventId;
+  
+      document.body.append(TI.cursorAddon);
+    }
+    TI.eventDefinitions.push([T.EVT_AC_SET, SetActionCursor, h_EVT_AC_SET]);
+
+    const h_EVT_AC_STOP = (data) => this._disposeAll();
+    TI.eventDefinitions.push([T.EVT_AC_STOP, IEvent, h_EVT_AC_STOP]);
+
+    super.init();
+  }
+
+  _mouseMove(e) {
+    if (!this.cursorAddon)
+      return;
+    
+    const offset = this.activeEvent.offsetLT;
+
+    this.cursorAddon.style.left = e.pageX + offset[0] + 'px';
+    this.cursorAddon.style.top  = e.pageY + offset[1] + 'px';
+  }
+
+  _fireEBEvent(e) {
+    if (this.activeEvent)
+      super._fireEBEvent(e);
+  }
+
+  _fillEventObject(x, e) {
+    super._fillEventObject(x, e);
+    x.id = this.activeEvent.convertedEventId;
+    x.senderId = this.aliasName;
+  }
+
+  deInit() {
+    this._disposeAll();
+    super.deInit();
+  }
+
+  _disposeAll() {
+    const TI = this;
+    TI.activeEvent = undefined;
+    TI.cursorAddon?.remove();
+    TI.cursorAddon = undefined;
+    TI.SEVT_MOUSE?.deInit();
+    TI.SEVT_MOUSE = undefined;
+  }
+}
+
+Plugins.catalogize(pServiceActionCursor);
