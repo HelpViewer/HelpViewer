2dadd11a761a329eafdcd1abd6d4bb09cf7e81ce
VSimon <vita.simon@centrum.cz>
2026-01-22 20:24:21 +0100

    pExportTEX : rules definition (partial; before refactor)

--- zip/plugins/pExportTEX.js
+++ zip/plugins/pExportTEX.js
@@ -10,6 +10,7 @@ class pExportTEX extends pExport {
   }
 
   async onETPrepareExport(evt) {
+    searchOverTextNodesAndDo(evt.parent, (x) => log('E TEXT NODE:', x.parentElement.nodeName, x));
     let document = await storageSearch(STO_DATA, this.cfgFILE, STOF_TEXT);
     // TODO : Resolve author name in export
     document = document.replace(/_AUTH_/g, '');
@@ -22,11 +23,35 @@ class pExportTEX extends pExport {
 
     let i_img = 0;
     let i_svg = 0;
-    for (const c of evt.data) {
-      let innerText = c.textContent.replace('#', '').trim();
-      // TODO: More clever sanitation !!
+    let parNode = evt.parent;
+    let codePre = undefined;
+    let liType = undefined;
+    let olulObject = undefined;
 
-      switch (c.tagName.toLowerCase()) {
+    searchOverTextNodesAndDo(evt.parent, (x) => {
+      if (parNode != x.parentElement) {
+        parNode = x.parentElement;
+      }
+
+      if (codePre) {
+        if (codePre == x.parentElement)
+          codePre = undefined;
+        else 
+          return;
+      }
+
+      //codePre = false;
+      log('E RR0 ', x.parentElement);
+      log('E RR ', [x, x.nodeName, x.parentElement.nodeName]);
+      const innerText = x.textContent;
+
+      if (liType && x.parentElement.nodeName.toLowerCase() != 'li') {
+        dataToFlush.push(`\\end{${liType}}`);
+        liType = undefined;
+        olulObject = undefined;
+      }
+
+      switch (x.parentElement.nodeName.toLowerCase()) {
         case 'p':
           dataToFlush.push(`${innerText}\\par`);
           break;
@@ -56,20 +81,64 @@ class pExportTEX extends pExport {
         case 'h6':
           dataToFlush.push(`\\subsubparagraph\{${innerText}\}`);
           break;
+        case 'div':
+          log('E DIV FOUND', x, x.nextElementSibling);
+          x = x.nextElementSibling;
+          if (Array.from(x.classList).find(cls => cls.startsWith('code-toolbar'))) {
+            codePre = x.nextElementSibling;
+            log('E GG', codePre);
+            const codeText = this.getPlainCodeFromPrism($O("code", x.childNodes[0]));
+            const prefix = 'language-';
+            const langClass = Array.from(x.classList).find(cls => cls.startsWith(prefix));
+            const lang = langClass ? langClass.replace(prefix, '') : 'text';
+            dataToFlush.push(`\\begin{lstlisting}[language=${lang}, caption={}]\n${codeText}\\end{lstlisting}`);
+          }
+          break;
         case 'pre':
-          const codeText = this.getPlainCodeFromPrism($O("code", c));
-          const prefix = 'language-';
-          const langClass = Array.from(c.classList).find(cls => cls.startsWith(prefix));
-          const lang = langClass ? langClass.replace(prefix, '') : 'text';
-          dataToFlush.push(`\\begin{lstlisting}[language=${lang}, caption={}]\n${codeText}\\end{lstlisting}`);
+          if (!codePre)
+            dataToFlush.push(`\\texttt{${innerText}}`);
+          break;
+        case 'a':
+          if (innerText.trim().length == 1)
+            break;
+          dataToFlush.push(`\\href{${x.parentElement.href}}{${innerText}}`);
           break;
         case 'ul':
+          if (olulObject)
+            break;
+          dataToFlush.push('\\begin{itemize}');
+          liType = 'itemize';
+          olulObject = x.parentElement;
           break;
         case 'ol':
+          if (olulObject)
+            break;
+          dataToFlush.push('\\begin{enumerate}');
+          liType = 'enumerate';
+          olulObject = x.parentElement;
           break;
-  
+        case 'strong':
+          dataToFlush.push(`\\textbf{${innerText}}`);
+          break;
+        case 'em':
+          dataToFlush.push(`\\emph{${innerText}}`);
+          break;
+        case 'li':
+          dataToFlush.push('\\item ');
+        default:
+          if (codePre)
+            break;
+
+          dataToFlush.push(`--L${innerText}L--`);
+          break;
       }
-    }
+    });
+    // for (const c of evt.data) {
+    //   let innerText = c.textContent.replace('#', '').trim();
+    //   // TODO: More clever sanitation !!
+
+
+    // }
     
     document = document.replace(/%DOC%/g, dataToFlush.join('\n'));
     dataToFlush = undefined;
