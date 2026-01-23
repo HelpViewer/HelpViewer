13b23bbebe07b3ae5302d2679286a625629d2474
VSimon <vita.simon@centrum.cz>
2026-01-23 16:34:41 +0100

    pExportTEX : metainfo table similar to puiButtonAsBook table added to TeX export

--- zip/plugins/pExportTEX.js
+++ zip/plugins/pExportTEX.js
@@ -144,6 +144,18 @@ class pExportTEX extends pExport {
     document = document.replace(/_LANG_/g, this.config[activeLanguage] || activeLanguage);
     document = document.replace(/_LSTSET_/g, this.config[`${activeLanguage}-lstset`] || '');
 
+    const metainfo = [
+      [ `:${_T('helpfile')}`, `:${_T('version')}` ],
+      [ configGetDataProjectFile(), configGetValue(CFG_KEY__VERSION) || '' ],
+      [ configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT), configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT) ],
+      [ _T('source'), dataPath],
+      [ _T('date'), getDateInYYYYMMDDHH24IIss(new Date())]
+    ];
+
+    const mColData = metainfo.map((x) => [ x[0].startsWith(':') ? `\\textbf{${x[0].substring(1)}}` : x[0], x[1].startsWith(':') ? `\\textbf{${x[1].substring(1)}}` : x[1] ]).map((x) => `${x[0]} & ${x[1]} \\\\`).join(' \\hline\n');
+
+    document = document.replace(/_METAINFO_/g, `\\begin{table}[H]\n\\begin{tabularx}{\\textwidth}{|l|X|}\n\\hline\n${mColData} \\hline\n \\end{tabularx}\\end{table}\n`);
+
     const ctx = { listStack: [], i_img: 0, i_svg: 0 };
     const latex = `\\section\{${header}\}\n` + walk(evt.parent, ctx);
 
