1aa24155a828b2691ef910c5864f0011322edea3
VSimon <vita.simon@centrum.cz>
2026-01-28 20:48:29 +0100

    pExportTEX : populate author variable

--- zip/plugins/pExportTEX.js
+++ zip/plugins/pExportTEX.js
@@ -18,10 +18,11 @@ class pExportTEX extends pExport {
       const header = getHeader();
       const activeLanguage = getActiveLanguage().toLowerCase();
       const config = this.config;
+      const author = configGetDataProjectFile();
     
       const metainfo = [
         [ `:${_T('helpfile')}`, `:${_T('version')}` ],
-        [ configGetDataProjectFile(), configGetValue(CFG_KEY__VERSION) || '' ],
+        [ author, configGetValue(CFG_KEY__VERSION) || '' ],
         [ configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT), configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT) ],
         [ _T('source'), dataPath],
         [ _T('date'), getDateInYYYYMMDDHH24IIss(new Date())]
@@ -32,7 +33,7 @@ class pExportTEX extends pExport {
       document = document.replace(/_METAINFO_/g, `\\begin{table}[H]\n\\begin{tabularx}{\\textwidth}{|l|X|}\n\\hline\n${mColData} \\hline\n \\end{tabularx}\\end{table}\n`);
   
       const ctx = { listStack: [], i_img: 0, i_svg: 0, embeds: evt.embeds };
-      const converted = HTMLToTeX(evt.parent, header, activeLanguage, config, ctx, document);
+      const converted = HTMLToTeX(evt.parent, header, activeLanguage, config, ctx, document, author);
       const latex = `\\section\{${header}\}\n` + converted[1];
       document = converted[0].replace(/%DOC%/g, latex);
       evt.output.file('LaTeX1.tex', document);
