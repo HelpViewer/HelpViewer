1c8a83622ff80bbd71ea902e391a0a847551d5a8
VSimon <vita.simon@centrum.cz>
2026-02-02 21:20:05 +0100

    puiButtonAsBook : optimization

--- zip/plugins/puiButtonAsBook.js
+++ zip/plugins/puiButtonAsBook.js
@@ -94,11 +94,8 @@ class puiButtonAsBook extends puiButtonTab {
           textOfFiles += `\n${fileAnchor}${y}\n`;
 
           if (isHomeData) {
-            const metainfo = `\n| ${_T('helpfile')} | ${_T('version')} |\n|---|---|\n| ${configGetDataProjectFile()} | ${configGetValue(CFG_KEY__VERSION) || ''} |\n| ${configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT)} | ${configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT)} |\n| ${_T('source')} | ${dataPath} |\n| ${_T('date')} | ${getDateInYYYYMMDDHH24IIss(new Date())} |\n\n`;
+            const metainfo = `\n| ${_T('helpfile')} | ${_T('version')} |\n|---|---|\n| ${configGetDataProjectFile()} | ${configGetValue(CFG_KEY__VERSION) || ''} |\n| ${configGetValue(CFG_KEY__PRJNAME, '', FILE_CONFIG_DEFAULT)} | ${configGetValue(CFG_KEY__VERSION, '', FILE_CONFIG_DEFAULT)} |\n| ${_T('source')} | ${dataPath} |\n| ${_T('date')} | ${getDateInYYYYMMDDHH24IIss(new Date())} |\n\n${PAGE_BREAK}`;
             textOfFiles += metainfo;
-            textOfFiles += PAGE_BREAK;
-            //textOfFiles += metainfo;
-            //textOfFiles += `\n<b>${_T('helpfile')}: ${configGetValue(CFG_KEY__PRJNAME)} <br>${_T('version')}: ${configGetValue(CFG_KEY__VERSION)}</b>`;
             const tree = $('tree');
             const data = tree?.outerHTML;
 
