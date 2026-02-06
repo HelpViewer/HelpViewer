1f254b274782ffd7be0e0b8e3138d586b058fbf1
VSimon <vita.simon@centrum.cz>
2026-02-06 18:31:10 +0100

    pTRParseMd : pExtensionMarkedMd extension plugin for marked and event MarkedExtend introduced for supporting customization and extension of marked.js parser, object explorer : EXTENSIONS plugin group introduced

---
+++ zip/plugins/pExtensionMarkedMd.js
@@ -0,0 +1,52 @@
+class pExtensionMarkedMd extends IPlugin {
+  constructor(aliasName, data) {
+    super(aliasName, data);
+    const TI = this;
+    TI.active = false;
+    TI.DEFAULT_KEY_CFG_LEVEL = 'block';
+  }
+
+  _getMarkedPluginName() {
+    return `${this.constructor.name}-${this.aliasName}`;
+  }
+
+  onET_MarkedExtend(e) {
+    const TI = this;
+    TI.active = true;
+
+    const handlingObject = {
+      name: TI._getMarkedPluginName(),
+      level: TI.cfgLEVEL,
+      start(src) { return TI.active ? TI._handlerStart(src) : undefined; },
+      tokenizer(src, ctx) {
+        if (!TI.active) return false;
+        return TI._handlerTokenizer(src, ctx);
+      },
+      renderer(token) {
+        return TI._handlerRenderer(token);
+      },
+    };
+
+    TI._initHandlingObject(handlingObject);
+    marked?.use({ extensions: [handlingObject] });
+  }
+
+  _initHandlingObject(o) {
+  }
+
+  _handlerStart(src) {
+  }
+
+  _handlerTokenizer(src, ctx) {
+  }
+
+  _handlerRenderer(token) {
+  }
+
+  deInit() {
+    this.active = false;
+    super.deInit();
+  }
+}
+
+Plugins.catalogize(pExtensionMarkedMd);
--- zip/plugins/pTRParseMd.js
+++ zip/plugins/pTRParseMd.js
@@ -1,17 +1,30 @@
 class pTRParseMd extends pTRPhasePlugin {
+  static EVT_MARKEDEXTEND = 'MarkedExtend';
+
   constructor(aliasName, data) {
     super(aliasName, data);
     this.DEFAULT_KEY_CFG_FILENAME = 'marked.min.js;LICENSE-marked.md';
   }
 
   init() {
+    const T = this.constructor;
+    const TI = this;
+
+    TI.eventDefinitions.push([T.EVT_MARKEDEXTEND, IEvent, null]); // outside event handlers
+    TI.catalogizeEventCall('onETShowChapterResolutions', T.EVT_MARKEDEXTEND);
     super.init();
-    this.RES_MARKED = new Resource('MARKED', undefined, STO_DATA, this.cfgFILENAME);
+
+    TI.RES_MARKED = new Resource('MARKED', undefined, STO_DATA, TI.cfgFILENAME);
   }
   
   onETShowChapterResolutions(r) {
-    if (!window.marked)
-      r.result = this.RES_MARKED?.init(r.result);
+    if (!window.marked) {
+      const T = this.constructor;
+      const TI = this;
+  
+      r.result = TI.RES_MARKED?.init(r.result);
+      r.result = r.result.then(() => sendEvent(T.EVT_MARKEDEXTEND));
+    }
 
     r.result = (!r.content || r.content.length == 0) ? r.result : r.result.then(() => r.content = marked.parse(r.content));
   }
