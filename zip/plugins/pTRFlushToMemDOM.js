1a1604f85e6dad3344113167bbaf73486529836d
VSimon <vita.simon@centrum.cz>
2026-02-15 19:00:07 +0100

    pTRFlushToMemDOM : bugfix of synchronous processing when it should continue with async partially processed content

--- zip/plugins/pTRFlushToMemDOM.js
+++ zip/plugins/pTRFlushToMemDOM.js
@@ -1,9 +1,11 @@
 class pTRFlushToMemDOM extends pTRPhasePlugin {
   onETShowChapterResolutions(r) {
-    log(`Rendering ${r.uri} data attached to memory DOM - still not visible to user for now`);
-    const parser = new DOMParser();
-    r.docM = parser.parseFromString(r.content, "text/html");
-    r.doc = r.docM;
+    r.result = r.result.then(() => {
+      log(`Rendering ${r.uri} data attached to memory DOM - still not visible to user for now`);
+      const parser = new DOMParser();
+      r.docM = parser.parseFromString(r.content, "text/html");
+      r.doc = r.docM;  
+    });
   }
 }
 
