7195f29ddfe398a44ee79211b946e89570e6ec9d
VSimon <vita.simon@centrum.cz>
2026-02-16 04:38:19 +0100

    pTRUnconnectedTransformation : bugfix of synchronous processing when it should continue with async partially processed content

--- zip/plugins/pTRUnconnectedTransformation.js
+++ zip/plugins/pTRUnconnectedTransformation.js
@@ -7,30 +7,31 @@ class pTRUnconnectedTransformation extends pTRPhasePlugin {
   onETShowChapterResolutions(r) {
     //relative link paths update for ZIP structure
     const checkList = ["href", "src", "data-href"];
-
-    $A('*', r.doc).forEach((el) => {
-      var val;
-      checkList.forEach((attr) => {
-        if (el.hasAttribute(attr)) {
-          val = el.getAttribute(attr);
-          if (val 
-            && el.tagName.toLowerCase() !== "img"
-            && !/^https?:\/\//.test(val) 
-            && !val.startsWith("mailto:") 
-            && !val.startsWith("tel:") 
-            && !val.startsWith("?") 
-            && !val.startsWith("#")) {
-            
-            const newVal = convertRelativePathToViewerURI(val);
-            el.setAttribute(attr, newVal);
-
-            if (!val.includes('.'))
-              val += this.cfgDEFAULTEXTENSION;
-            
-            if (!val?.startsWith('?d') && !el.getAttribute('data-param'))
-              el.setAttribute('data-param', val);    
+    r.result = (r.result || Promise.resolve()).then(() => {
+      $A('*', r.doc).forEach((el) => {
+        var val;
+        checkList.forEach((attr) => {
+          if (el.hasAttribute(attr)) {
+            val = el.getAttribute(attr);
+            if (val 
+              && el.tagName.toLowerCase() !== "img"
+              && !/^https?:\/\//.test(val) 
+              && !val.startsWith("mailto:") 
+              && !val.startsWith("tel:") 
+              && !val.startsWith("?") 
+              && !val.startsWith("#")) {
+              
+              const newVal = convertRelativePathToViewerURI(val);
+              el.setAttribute(attr, newVal);
+  
+              if (!val.includes('.'))
+                val += this.cfgDEFAULTEXTENSION;
+              
+              if (!val?.startsWith('?d') && !el.getAttribute('data-param'))
+                el.setAttribute('data-param', val);    
+            }
           }
-        }
+        });
       });
     });
 
